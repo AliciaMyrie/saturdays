50709b26af17afe4b541e4d5d53e6ef054e53e67
Alicia Myrie <aliciamyrie@Alicias-MacBook-Air.local>
2022-07-16 16:01:21 -0400

    added shopping list loop

---
+++ shopping.js
@@ -0,0 +1,36 @@
+//You are given 2 arrays, onecalled shoppingList and one called cart.
+//Create a new version of shoppingList where the items in the cart have been removed.
+
+let shoppingList = [
+  "beer",
+  "milk",
+  "eggs",
+  "bread",
+  "ham",
+  "cheese",
+  "mayo",
+  "mustard",
+];
+let cart = ["milk", "bread", "cheese", "mustard"];
+let finalList = [];
+
+// loop first array
+// grab each element on first array
+// create a variable that keep strack f we found it in the second array
+// loop through second array
+// check wether the element on the first array exists in the second array
+// if it does not exist in any element of the swcond array
+// push it to the final list
+
+let shoppingList = [
+  "beer",
+  "milk",
+  "eggs",
+  "bread",
+  "ham",
+  "cheese",
+  "mayo",
+  "mustard",
+];
+let cart = ["milk", "bread", "cheese", "mustard"];
+let finalList = [];
